acfdbc4e028c7fdc140d7bc5f106d2559c302513
John Wohlbier <john.wohlbier@engilitycorp.com>
2018-09-11 21:17:32 -0400

    REST Implementations for basic CRUD on Users table.

--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var sql = require("mssql");
-var app = express(); 
+var app = express();
 
-// Body Parser Middleware
-app.use(bodyParser.json()); 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //CORS Middleware
 app.use(function (req, res, next) {
-  //Enabling CORS 
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
@@ -34,47 +36,80 @@ var dbConfig = {
 };
 
 //Function to connect to database and execute query
-var  executeQuery = function(res, query){             
+var executeQuery = function(res, query) {
   sql.connect(dbConfig, function (err) {
-    if (err) {   
+    if (err) {
       console.log("Error while connecting database :- " + err);
       res.send(err);
+      sql.close();
     } else {
       // create Request object
       var request = new sql.Request();
+
       // query to the database
       request.query(query, function (err, result) {
         if (err) {
 	  console.log("Error while querying database :- " + err);
 	  res.send(err);
+	  sql.close();
 	} else {
 	  res.send(result);
+	  sql.close();
 	}
      });
     }
-  });           
+  });
 }
 
 //GET API
-app.get("/api/user", function(req , res){
-  var query = "select * from [user]";
+app.get("/Users", function(req, res) {
+  var query = "SELECT * FROM [Users]";
   executeQuery (res, query);
 });
 
-//POST API
-//app.post("/api/user", function(req , res){
-//                var query = "INSERT INTO [user] (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password”);
-//                executeQuery (res, query);
-//});
+//GET API
+app.get("/Users/:id", function(req, res) {
+  var query = "SELECT * FROM [Users] WHERE Id=" + req.params.id;
+  executeQuery (res, query);
+});
 
-//PUT API
-//app.put("/api/user/:id", function(req , res){
-//              var query = "UPDATE [user] SET Name= " + req.body.Name  +  " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
-//              executeQuery (res, query);
-//});
+
+//POST API
+// EMAIL
+// PHONE
+// PASSWORD
+// NAME
+// BIRTH_MONTH
+// BIRTH_YEAR
+// ADDRESS
+// CITY
+// STATE
+// ZIPCODE
+// DELETED
+// VERSION
+// CREATEDAT
+// UPDATEDAT
+app.post("/Users", function(req , res) {
+  var query = "INSERT INTO [Users] \
+    ([email],[phone]) \
+    VALUES ('"
+    + req.body.email + "','"
+    + req.body.phone + "')";
+
+  executeQuery (res, query);
+});
+
+//PUT/PATCH API
+app.put("/Users/:id", function(req, res) {
+  var query = "UPDATE [Users] SET \
+    email='" + req.body.email
+    + "', phone='" + req.body.phone
+    + "' WHERE Id=" + req.params.id;
+  executeQuery (res, query);
+});
 
 // DELETE API
-//app.delete("/api/user /:id", function(req , res){
-//               var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
-//               executeQuery (res, query);
-//});
+app.delete("/Users/:id", function(req , res) {
+  var query = "DELETE FROM [Users] WHERE Id=" + req.params.id;
+  executeQuery (res, query);
+});
