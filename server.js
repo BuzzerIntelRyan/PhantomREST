beb6819b5458b94b87aa79acd6a50dc29ed576a1
John Wohlbier <john.wohlbier@engilitycorp.com>
2018-09-17 20:44:20 -0400

    remove authorization access control allow header

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
   //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Origin, X-Requested-With,  ");
+  res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Origin, X-Requested-With,  ");
   next();
 });
 
